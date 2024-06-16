6ca437aad9fad70666c9c21ab8849a0e46dfd543
n-jc <134266319+n-jc@users.noreply.github.com>
2024-06-16 23:00:58 +1000

    public JS Action #8

--- dist/index.js
+++ dist/index.js
@@ -29200,7 +29200,7 @@ const github = __nccwpck_require__(5438)
  */
 async function run() {
   try {
-    const token = core.getInput('token')
+    const token = core.getInput('token', { required: true })
     const title = core.getInput('title')
     const body = core.getInput('body')
     const assignees = core.getInput('assignees')
